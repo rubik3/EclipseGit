185928f255937330aa381834ab52e1d6be68a6b8
Yeray <comodin26@gmail.com>
2019-04-16 09:10:53 +0200

    Cambiado el adios mundo

--- SaludaMundo/src/Mundo/AdiosMundo.java
+++ SaludaMundo/src/Mundo/AdiosMundo.java
@@ -5,7 +5,7 @@ public class AdiosMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Adios Mundo Cruel");
+		System.out.println("Adios Mundo  Cruel :)");
 	}
 
 }
